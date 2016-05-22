c6c4e7d206c3800b651379025d949ccc032470ac
unknown <001387@zjzxlh.cn>
2016-05-22 18:36:37 +0800

    index

---
+++ src/main/java/com/yy/control/CustomerControl.java
@@ -0,0 +1,11 @@
+package com.yy.control;
+
+/**
+ * @ClassName: CustomerControl
+ * @Description: 客户信息控制器
+ * @author caizhen
+ * @date 2016年5月22日 下午5:19:36
+ */
+public class CustomerControl {
+
+}
