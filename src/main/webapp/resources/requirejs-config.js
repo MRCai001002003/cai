8477f8854101c9fecbfce7561e157317010b751d
unknown <001387@zjzxlh.cn>
2016-05-23 19:22:25 +0800

    change code

--- src/main/webapp/resources/requirejs-config.js
+++ src/main/webapp/resources/requirejs-config.js
@@ -5,11 +5,8 @@ require.config({
         'ui.router': '../library/angular-ui-router/release/angular-ui-router.min',
         'angular.animate': '../library/angular-animate/angular-animate.min',
         'angular.sanitize': '../library/angular-sanitize/angular-sanitize.min',
-		'angular.validate':'../library/angular-w5c-validator/w5cValidator.min',
         'iScroll': '../library/iscroll/build/iscroll-probe.min',
         'hammer': '../library/hammerjs/hammer.min',
-		'wxSDK': 'https://res.wx.qq.com/open/js/jweixin-1.0.0',
-		'd3':'../library/d3/d3.min'
     },
     urlArgs: 't=20160425',// + (new Date).getTime(),
     shim: {
@@ -25,6 +22,5 @@ require.config({
         'ui.router': ['angular'],
         'angular.animate': ['angular'],
         'angular.sanitize': ['angular'],
-        'angular.validate': ['angular']
     }
 });
