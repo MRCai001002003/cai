c555ae80a256e3f3001d3ba72208fb791bdf06c5
unknown <001387@zjzxlh.cn>
2016-05-23 19:07:28 +0800

    aa

--- src/main/java/com/yy/control/CustomerControl.java
+++ src/main/java/com/yy/control/CustomerControl.java
@@ -1,11 +1,14 @@
 package com.yy.control;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @ClassName: CustomerControl
  * @Description: 客户信息控制器
  * @author caizhen
  * @date 2016年5月22日 下午5:19:36
  */
+@Controller
 public class CustomerControl {
 
 }
